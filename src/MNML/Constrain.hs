105d7983b6cfe7bd8f749b17daf50e5b9f9c5d4d
Jonathan Arnett <jonarnett90@gmail.com>
2024-10-26 19:07:33 -0400

    Extract "Constrain" out of "Unify"

---
+++ src/MNML/Constrain.hs
@@ -0,0 +1,233 @@
+module MNML.Constrain
+    ( ConstraintError
+    , valueConstraints
+    ) where
+
+import           Control.Monad.State (State, StateT, evalStateT, gets, lift,
+                                      modify, state)
+import           Data.Bifunctor      (first)
+import           Data.Map            (Map, (!?))
+import qualified Data.Map            as Map
+import           Data.Maybe          (listToMaybe, mapMaybe)
+import           Data.Text           (Text)
+import           Lens.Micro          (Lens', lens, over, set)
+import           MNML                (CompilerState (..))
+import qualified MNML.AST            as AST
+import qualified MNML.Parse          as P
+import qualified MNML.Type           as T
+
+type Bindings = Map Text T.Type
+
+data ConstraintError
+  = UnknownVar AST.NodeId
+  | UnknownVal Text
+  | UnknownConstructor Text
+  | UnknownType Text -- AST.NodeId
+  | ArgumentLengthMismatch AST.NodeId
+  | OccursError T.Type T.Type AST.NodeId
+  | ExpectedTraits T.Type [T.Trait] AST.NodeId
+  | ParseError P.ParseError
+  deriving (Eq, Show)
+
+data ConstrainEnv
+  = ConstrainEnv
+      { _bindings  :: Bindings
+      , _module    :: Text
+      , _nextVarId :: T.VarId
+      , _errors    :: [ConstraintError]
+      }
+
+initialEnv :: Text -> ConstrainEnv
+initialEnv modu =
+  ConstrainEnv
+    { _bindings = Map.empty,
+      _module = modu,
+      _nextVarId = 0,
+      _errors = []
+    }
+
+bindings :: Lens' ConstrainEnv Bindings
+bindings = lens _bindings (\us bin -> us {_bindings = bin})
+
+type Constrain a = StateT ConstrainEnv (State CompilerState) a
+
+addError :: ConstraintError -> Constrain ()
+addError err = modify (\s -> s {_errors = err : _errors s})
+
+giveUp :: ConstraintError -> Text -> Constrain (T.Type, [T.Constraint])
+giveUp err name = addError err >> (,[]) <$> freshTypeVar name []
+
+constrain :: AST.Expr -> Constrain (T.Type, [T.Constraint])
+constrain (AST.EVar name nodeId) = do
+  lookupRes <- gets ((!? name) . _bindings)
+  case lookupRes of
+    -- The presence of a var doesn't inform it's type
+    Just t -> return (t, [])
+    Nothing -> do
+      addError (UnknownVar nodeId)
+      newTVar <- freshTypeVar name []
+      return (newTVar, [])
+constrain (AST.EConstructor name _nodeId) = do
+  modu <- gets _module
+  expectedTypeRes <- constructorFunType modu name
+  case expectedTypeRes of
+    Left err           -> giveUp err "name"
+    Right expectedType -> return (expectedType, [])
+constrain (AST.ELit lit _nodeId) =
+  (,[]) <$> litType lit
+constrain (AST.ELambda args body nodeId) = do
+  oldBindings <- gets _bindings
+  -- Fresh argument type bindings
+  argVars <- mapM (\arg -> (arg,) <$> freshTypeVar arg []) args
+  modify (over bindings (Map.union (Map.fromList argVars)))
+  -- Infer body constraints
+  (bt, bc) <- constrain body
+  -- Restore bindings
+  modify (set bindings oldBindings)
+  --
+  retType <- freshTypeVar "fun" []
+  return (retType, T.CEqual retType (T.Fun (map snd argVars) bt) nodeId : bc)
+constrain (AST.EApp funExpr argExprs nodeId) = do
+  (funType, fc) <- constrain funExpr
+  argResults <- mapM constrain argExprs
+  let argTypes = map fst argResults
+      argConstraints = concatMap snd argResults
+  retType <- freshTypeVar "ret" []
+  return (retType, T.CEqual (T.Fun argTypes retType) funType nodeId : fc ++ argConstraints)
+-- constrain (AST.ECase subj branches _nodeId) = do
+--   (subjType, subjConstraints) <- constrain subj
+--   -- return ()
+-- _
+
+constrain (AST.EBinary _op left right nodeId) = do
+  (lType, lConstraints) <- constrain left
+  (rType, rConstraints) <- constrain right
+  retVar <- freshTypeVar "ret" [T.Numeric]
+  let lConstraint = T.CEqual retVar lType nodeId
+      rConstraint = T.CEqual retVar rType nodeId
+  return (retVar, lConstraint : rConstraint : lConstraints ++ rConstraints)
+constrain (AST.ERecord fields nodeId) = do
+  fieldResults <- mapM constrain (snd <$> fields)
+  let typedFields = zip (fst <$> fields) (fst <$> fieldResults)
+      fieldConstraints = concatMap snd fieldResults
+  retType <- freshTypeVar "ret" []
+  return (retType, T.CEqual retType (T.Record typedFields) nodeId : fieldConstraints)
+constrain (AST.EList elems nodeId) = do
+  elemResults <- mapM constrain elems
+  elemType <- freshTypeVar "elem" []
+  retType <- freshTypeVar "ret" []
+  let consistencyConstraints = (\t -> T.CEqual elemType t nodeId) . fst <$> elemResults
+      elemConstraints = concatMap snd elemResults
+  return (retType, T.CEqual (T.List elemType) retType nodeId : consistencyConstraints ++ elemConstraints)
+
+freshTypeVar :: Text -> [T.Trait] -> Constrain T.Type
+freshTypeVar name traits = state (freshTypeVar' name traits)
+  where
+    freshTypeVar' n ts s =
+      let newTVId = _nextVarId s
+       in (T.Var n ts newTVId, s {_nextVarId = newTVId + 1})
+
+litType :: AST.Literal -> Constrain T.Type
+-- For now, an "int" literal (e.g. "5") will be interpreted as "numeric type" (could be float)
+litType (AST.LInt _ _)    = freshTypeVar "num" [T.Numeric]
+litType (AST.LFloat _ _)  = return T.Float
+litType (AST.LChar _ _)   = return T.Char
+litType (AST.LString _ _) = return T.String
+
+constructorFunType :: Text -> Text -> Constrain (Either ConstraintError T.Type)
+constructorFunType modu conName = do
+  mDef <- lift (moduleDef modu)
+  case mDef of
+    Left err    -> return $ Left err
+    Right decls -> findConType decls
+  where
+    findConType :: [AST.Declaration] -> Constrain (Either ConstraintError T.Type)
+    findConType decls =
+      foldl (<>) (Left (UnknownConstructor conName))
+        <$> mapM
+          ( \case
+              (AST.TypeDecl tName constructors _) -> conTypeFromConstructors conName tName constructors
+              _ -> (return (Left (UnknownConstructor conName)))
+          )
+          decls
+
+conTypeFromConstructors :: Text -> Text -> [(Text, [AST.Type])] -> Constrain (Either ConstraintError T.Type)
+conTypeFromConstructors conName tName constructors = do
+  foldl (<>) (Left (UnknownConstructor conName))
+    <$> mapM
+      ( \case
+          (cName, argTypes) | cName == conName -> typifyConstructor argTypes tName
+          _ -> return (Left (UnknownConstructor conName))
+      )
+      constructors
+
+typifyConstructor :: [AST.Type] -> Text -> Constrain (Either ConstraintError T.Type)
+typifyConstructor argTypes tName = do
+  typifiedTypes <- mapM typify argTypes
+  return $ T.Fun <$> sequence typifiedTypes <*> return (T.AlgebraicType tName)
+
+findMaybe :: (a -> Maybe b) -> [a] -> Maybe b
+findMaybe f = listToMaybe . mapMaybe f
+
+typify :: AST.Type -> Constrain (Either ConstraintError T.Type)
+typify (AST.TInt _) = return (Right T.Int)
+typify (AST.TFloat _) = return (Right T.Float)
+typify (AST.TChar _) = return (Right T.Char)
+typify (AST.TString _) = return (Right T.String)
+typify (AST.TNamedType name _) = do
+  modName <- gets _module
+  moduleNamedType modName name
+typify (AST.TList t _) = (T.List <$>) <$> typify t
+typify (AST.TFun argTypes resType _) = do
+  maybeArgTypes <- mapM typify argTypes
+  maybeResType <- typify resType
+  return (T.Fun <$> sequence maybeArgTypes <*> maybeResType)
+-- There might be a more elegant way, not sure
+typify (AST.TRecord fields _) = do
+  fieldTypes <- mapM (\(name, aType) -> ((name,) <$>) <$> typify aType) fields
+  return $ T.Record <$> sequence fieldTypes
+typify (AST.TVar name _) = Right <$> freshTypeVar name []
+
+moduleNamedType :: Text -> Text -> Constrain (Either ConstraintError T.Type)
+moduleNamedType modu typeName = do
+  mDefRes <- lift (moduleDef modu)
+  case mDefRes of
+    Right mDef -> findDef typeName mDef
+    Left _     -> return (Left (UnknownType typeName))
+  where
+    findDef :: Text -> [AST.Declaration] -> Constrain (Either ConstraintError T.Type)
+    findDef name decls =
+      foldl (<>) (Left (UnknownType typeName))
+        <$> mapM
+          ( \case
+              (AST.TypeDecl n _ _) | n == name -> return (Right (T.AlgebraicType n))
+              (AST.TypeAliasDecl n t _) | n == name -> typify t
+              _ -> return (Left (UnknownType typeName))
+          )
+          decls
+
+valueConstraints :: Text -> Text -> State CompilerState (Either ConstraintError (T.Type, [T.Constraint]))
+valueConstraints modu valName = do
+  def <- valueDef modu valName
+  case def of
+    Right expr -> Right <$> evalStateT (constrain expr) (initialEnv modu)
+    Left err   -> return (Left err)
+
+valueDef :: Text -> Text -> State CompilerState (Either ConstraintError AST.Expr)
+valueDef modu valName = do
+  mDef <- moduleDef modu
+  return $ mDef >>= findDef valName
+  where
+    findDef name decls =
+      maybe (Left $ UnknownVal name) Right $
+        findMaybe
+          ( \case
+              (AST.ValueDecl n expr _) | n == name -> Just expr
+              _ -> Nothing
+          )
+          decls
+
+-- TODO: Cache module definitions here
+moduleDef :: Text -> State CompilerState (Either ConstraintError [AST.Declaration])
+moduleDef modu = do
+  first ParseError <$> P.parse modu
--- src/MNML/Unify.hs
+++ src/MNML/Unify.hs
@@ -3,18 +3,17 @@ module MNML.Unify
     , valueType
     ) where
 
-import           Control.Monad.State (State, StateT, evalStateT, gets, lift,
-                                      modify, runState, state)
-import           Data.Bifunctor      (first, second)
+import           Control.Monad.State (State, modify, runState)
+import           Data.Bifunctor      (second)
 import           Data.Function       (on)
 import qualified Data.List           as List
 import           Data.Map            (Map, (!?))
 import qualified Data.Map            as Map
-import           Data.Maybe          (fromMaybe, listToMaybe, mapMaybe)
+import           Data.Maybe          (fromMaybe)
 import           Data.Text           (Text)
-import           Lens.Micro          (Lens', lens, over, set)
 import           MNML                (CompilerState (..))
 import qualified MNML.AST            as AST
+import qualified MNML.Constrain      as C
 import qualified MNML.Parse          as P
 import qualified MNML.Type           as T
 
@@ -27,115 +26,9 @@ data UnificationError
   | UnificationError T.Type T.Type AST.NodeId
   | OccursError T.Type T.Type AST.NodeId
   | ExpectedTraits T.Type [T.Trait] AST.NodeId
+  | ConstraintError C.ConstraintError
   | ParseError P.ParseError
   deriving (Eq, Show)
-
-type Bindings = Map Text T.Type
-
-data UnificationState
-  = UnificationState
-      { _bindings  :: Bindings
-      , _module    :: Text
-      , _nextVarId :: T.VarId
-      , _errors    :: [UnificationError]
-      }
-
-initialEnv :: Text -> UnificationState
-initialEnv modu =
-  UnificationState
-    { _bindings = Map.empty,
-      _module = modu,
-      _nextVarId = 0,
-      _errors = []
-    }
-
-type Unify a = StateT UnificationState (State CompilerState) a
-
-bindings :: Lens' UnificationState Bindings
-bindings = lens _bindings (\us bin -> us {_bindings = bin})
-
-addError :: UnificationError -> Unify ()
-addError err = modify (\s -> s {_errors = err : _errors s})
-
-giveUp :: UnificationError -> Text -> Unify (T.Type, [T.Constraint])
-giveUp err name = addError err >> (,[]) <$> freshTypeVar name []
-
-constrain :: AST.Expr -> Unify (T.Type, [T.Constraint])
-constrain (AST.EVar name nodeId) = do
-  lookupRes <- gets ((!? name) . _bindings)
-  case lookupRes of
-    -- The presence of a var doesn't inform it's type
-    Just t -> return (t, [])
-    Nothing -> do
-      addError (UnknownVar nodeId)
-      newTVar <- freshTypeVar name []
-      return (newTVar, [])
-constrain (AST.EConstructor name _nodeId) = do
-  modu <- gets _module
-  expectedTypeRes <- constructorFunType modu name
-  case expectedTypeRes of
-    Left err           -> giveUp err "name"
-    Right expectedType -> return (expectedType, [])
-constrain (AST.ELit lit _nodeId) =
-  (,[]) <$> litType lit
-constrain (AST.ELambda args body nodeId) = do
-  oldBindings <- gets _bindings
-  -- Fresh argument type bindings
-  argVars <- mapM (\arg -> (arg,) <$> freshTypeVar arg []) args
-  modify (over bindings (Map.union (Map.fromList argVars)))
-  -- Infer body constraints
-  (bt, bc) <- constrain body
-  -- Restore bindings
-  modify (set bindings oldBindings)
-  --
-  retType <- freshTypeVar "fun" []
-  return (retType, T.CEqual retType (T.Fun (map snd argVars) bt) nodeId : bc)
-constrain (AST.EApp funExpr argExprs nodeId) = do
-  (funType, fc) <- constrain funExpr
-  argResults <- mapM constrain argExprs
-  let argTypes = map fst argResults
-      argConstraints = concatMap snd argResults
-  retType <- freshTypeVar "ret" []
-  return (retType, T.CEqual (T.Fun argTypes retType) funType nodeId : fc ++ argConstraints)
--- constrain (AST.ECase subj branches _nodeId) = do
---   (subjType, subjConstraints) <- constrain subj
---   -- return ()
--- _
-
-constrain (AST.EBinary _op left right nodeId) = do
-  (lType, lConstraints) <- constrain left
-  (rType, rConstraints) <- constrain right
-  retVar <- freshTypeVar "ret" [T.Numeric]
-  let lConstraint = T.CEqual retVar lType nodeId
-      rConstraint = T.CEqual retVar rType nodeId
-  return (retVar, lConstraint : rConstraint : lConstraints ++ rConstraints)
-constrain (AST.ERecord fields nodeId) = do
-  fieldResults <- mapM constrain (snd <$> fields)
-  let typedFields = zip (fst <$> fields) (fst <$> fieldResults)
-      fieldConstraints = concatMap snd fieldResults
-  retType <- freshTypeVar "ret" []
-  return (retType, T.CEqual retType (T.Record typedFields) nodeId : fieldConstraints)
-constrain (AST.EList elems nodeId) = do
-  elemResults <- mapM constrain elems
-  elemType <- freshTypeVar "elem" []
-  retType <- freshTypeVar "ret" []
-  let consistencyConstraints = (\t -> T.CEqual elemType t nodeId) . fst <$> elemResults
-      elemConstraints = concatMap snd elemResults
-  return (retType, T.CEqual (T.List elemType) retType nodeId : consistencyConstraints ++ elemConstraints)
-
-freshTypeVar :: Text -> [T.Trait] -> Unify T.Type
-freshTypeVar name traits = state (freshTypeVar' name traits)
-  where
-    freshTypeVar' n ts s =
-      let newTVId = _nextVarId s
-       in (T.Var n ts newTVId, s {_nextVarId = newTVId + 1})
-
-litType :: AST.Literal -> Unify T.Type
--- For now, an "int" literal (e.g. "5") will be interpreted as "numeric type" (could be float)
-litType (AST.LInt _ _)    = freshTypeVar "num" [T.Numeric]
-litType (AST.LFloat _ _)  = return T.Float
-litType (AST.LChar _ _)   = return T.Char
-litType (AST.LString _ _) = return T.String
 
 type Subst = Map T.Type T.Type
 
@@ -233,110 +126,12 @@ applySubst subst (T.List elemType) = T.List (applySubst subst elemType)
 applySubst subst (T.Fun argTypes retType) = T.Fun (applySubst subst <$> argTypes) (applySubst subst retType)
 applySubst subst (T.Record fieldSpec) = T.Record $ second (applySubst subst) <$> fieldSpec
 
--- I think the play here is to use these State CompilerState ... to implement the cache (by allowing them to populate a cache in CompilerState)
-
 valueType :: Text -> Text -> State CompilerState (Either UnificationError T.Type)
 valueType modu valName = do
-  valDef <- valueDef modu valName
-  case valDef of
-    Right expr ->
-      evalStateT
-        ( do
-            (inferType, constraints) <- constrain expr
-            case runState (unify constraints) Map.empty of
-              (Just err, _) -> return (Left err)
-              (Nothing, subst) -> return (Right (fromMaybe inferType (subst !? inferType)))
-        )
-        (initialEnv modu)
-    Left parseErr -> return (Left parseErr)
-
-valueDef :: Text -> Text -> State CompilerState (Either UnificationError AST.Expr)
-valueDef modu valName = do
-  mDef <- moduleDef modu
-  return $ mDef >>= findDef valName
-  where
-    findDef name decls =
-      maybe (Left $ UnknownVal name) Right $
-        findMaybe
-          ( \case
-              (AST.ValueDecl n expr _) | n == name -> Just expr
-              _ -> Nothing
-          )
-          decls
-
-constructorFunType :: Text -> Text -> Unify (Either UnificationError T.Type)
-constructorFunType modu conName = do
-  mDef <- lift (moduleDef modu)
-  case mDef of
-    Left err    -> return $ Left err
-    Right decls -> findConType decls
-  where
-    findConType :: [AST.Declaration] -> Unify (Either UnificationError T.Type)
-    findConType decls =
-      foldl (<>) (Left (UnknownConstructor conName))
-        <$> mapM
-          ( \case
-              (AST.TypeDecl tName constructors _) -> conTypeFromConstructors conName tName constructors
-              _ -> (return (Left (UnknownConstructor conName)))
-          )
-          decls
-
-conTypeFromConstructors :: Text -> Text -> [(Text, [AST.Type])] -> Unify (Either UnificationError T.Type)
-conTypeFromConstructors conName tName constructors = do
-  foldl (<>) (Left (UnknownConstructor conName))
-    <$> mapM
-      ( \case
-          (cName, argTypes) | cName == conName -> typifyConstructor argTypes tName
-          _ -> return (Left (UnknownConstructor conName))
-      )
-      constructors
-
-typifyConstructor :: [AST.Type] -> Text -> Unify (Either UnificationError T.Type)
-typifyConstructor argTypes tName = do
-  typifiedTypes <- mapM typify argTypes
-  return $ T.Fun <$> sequence typifiedTypes <*> return (T.AlgebraicType tName)
-
-findMaybe :: (a -> Maybe b) -> [a] -> Maybe b
-findMaybe f = listToMaybe . mapMaybe f
-
-typify :: AST.Type -> Unify (Either UnificationError T.Type)
-typify (AST.TInt _) = return (Right T.Int)
-typify (AST.TFloat _) = return (Right T.Float)
-typify (AST.TChar _) = return (Right T.Char)
-typify (AST.TString _) = return (Right T.String)
-typify (AST.TNamedType name _) = do
-  modName <- gets _module
-  moduleNamedType modName name
-typify (AST.TList t _) = (T.List <$>) <$> typify t
-typify (AST.TFun argTypes resType _) = do
-  maybeArgTypes <- mapM typify argTypes
-  maybeResType <- typify resType
-  return (T.Fun <$> sequence maybeArgTypes <*> maybeResType)
--- There might be a more elegant way, not sure
-typify (AST.TRecord fields _) = do
-  fieldTypes <- mapM (\(name, aType) -> ((name,) <$>) <$> typify aType) fields
-  return $ T.Record <$> sequence fieldTypes
-typify (AST.TVar name _) = Right <$> freshTypeVar name []
-
-moduleNamedType :: Text -> Text -> Unify (Either UnificationError T.Type)
-moduleNamedType modu typeName = do
-  mDefRes <- lift (moduleDef modu)
-  case mDefRes of
-    Right mDef -> findDef typeName mDef
-    Left _     -> return (Left (UnknownType typeName))
-  where
-    findDef :: Text -> [AST.Declaration] -> Unify (Either UnificationError T.Type)
-    findDef name decls =
-      foldl (<>) (Left (UnknownType typeName))
-        <$> mapM
-          ( \case
-              (AST.TypeDecl n _ _) | n == name -> return (Right (T.AlgebraicType n))
-              (AST.TypeAliasDecl n t _) | n == name -> typify t
-              _ -> return (Left (UnknownType typeName))
-          )
-          decls
-
--- TODO: Cache module definitions here
-moduleDef :: Text -> State CompilerState (Either UnificationError [AST.Declaration])
-moduleDef modu = do
-  first ParseError <$> P.parse modu
+  constraintsRes <- C.valueConstraints modu valName
+  case constraintsRes of
+    Right (inferType, constraints) ->
+      case runState (unify constraints) Map.empty of
+        (Just err, _) -> return (Left err)
+        (Nothing, subst) -> return (Right (fromMaybe inferType (subst !? inferType)))
+    Left constrainErr -> return (Left (ConstraintError constrainErr))
