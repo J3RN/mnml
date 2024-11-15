4e46813b791aa455d326ce10c1028d2b9915ace7
Jonathan Arnett <jonarnett90@gmail.com>
2024-11-15 18:12:07 -0500

    Format Core

--- src/MNML/Core.hs
+++ src/MNML/Core.hs
@@ -1,43 +1,60 @@
 -- An IR that resembles WASM
-module MNML.Core (Expr (..), Function (..), Modu (..), Memory (..), Type (..)) where
+module MNML.Core
+    ( Expr (..)
+    , Function (..)
+    , Memory (..)
+    , Modu (..)
+    , Type (..)
+    ) where
 
-import Data.Text (Text)
+import           Data.Text (Text)
 
-newtype Funcref = Funcref Text
-data Expr = Expr { _callee :: Text
-                 , _args :: [Either Text Expr]
-                 }
+newtype Funcref
+  = Funcref Text
+data Expr
+  = Expr
+      { _callee :: Text
+      , _args   :: [Either Text Expr]
+      }
 
 -- Ignoring vector types for the time being
-data Type = I32 | F64 | FR Funcref
+data Type
+  = I32
+  | F64
+  | FR Funcref
 
-data Function = Function { _params :: [Type]
-                         , _locals :: [Type]
-                         , _return :: Type
-                         , _body :: [Expr]
-                         }
+data Function
+  = Function
+      { _params :: [Type]
+      , _locals :: [Type]
+      , _return :: Type
+      , _body   :: [Expr]
+      }
 
-newtype Memory = Memory (Maybe Int)
+newtype Memory
+  = Memory (Maybe Int)
 
-data Modu = Modu { memories :: [Memory]
-                 , functions :: [Function]
-                 }
+data Modu
+  = Modu
+      { memories  :: [Memory]
+      , functions :: [Function]
+      }
 
 instance Show Expr where
   show e = concat $ "(" : show (_callee e) : map showArg (_args e) ++ [")"]
-    where showArg (Left t) = show t
+    where showArg (Left t)   = show t
           showArg (Right e') = show e'
 
 instance Show Type where
-  show I32 = "i32"
-  show F64 = "f64"
+  show I32                 = "i32"
+  show F64                 = "f64"
   show (FR (Funcref name)) = concat ["(func ", show name, ")"]
 
 instance Show Function where
   show f = concat $ "(func " : map show (_params f) ++ map show (_locals f) ++ [show (_return f)] ++ map show (_body f) ++ [")"]
 
 instance Show Memory where
-  show (Memory Nothing) = "(memory)"
+  show (Memory Nothing)     = "(memory)"
   show (Memory (Just size)) = concat ["(memory", show size, ")"]
 
 instance Show Modu where
