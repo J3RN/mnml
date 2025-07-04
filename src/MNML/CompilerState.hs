5f6c34600d36189ea1d2f4487e768a35509bc2aa
Jonathan Arnett <j3rn@j3rn.com>
2025-07-03 22:18:52 -0400

    Get CompilerState into a more complete form

--- src/MNML/CompilerState.hs
+++ src/MNML/CompilerState.hs
@@ -1,50 +1,63 @@
 module MNML.CompilerState
     ( CompilerState (..)
-    , definitions
     , emptyState
+    , lookupType
+    , lookupVal
     , nextTypeId
-    , typeDefinitions
     , varIdPlusPlus
     ) where
 
 import           Control.Monad.State (State, state)
-import           Data.Map            (Map)
+import           Data.Map            (Map, (!?))
 import qualified Data.Map            as Map
 import           Lens.Micro          (Lens', lens)
-
 import qualified MNML.AST.Type       as TAST
 import           MNML.Base           (QualifiedReference)
-import qualified MNML.Type           as Type
+import qualified MNML.Type           as T
 
 -- The compiler state contains caches and the type variable counter
 
+type Identifier = String
+
 data CompilerState
   = CompilerState
-      { _definitions     :: Map QualifiedReference TAST.Expr
-      , _typeDefinitions :: Map QualifiedReference Type.Type
-      , _nextTypeId      :: Type.VarId
+      { _valueNames       :: Map QualifiedReference Identifier
+      , _valueDefinitions :: Map Identifier TAST.Expr
+      , _typeNames        :: Map QualifiedReference Identifier
+      , _typeDefinitions  :: Map Identifier T.Type
+      , _nextTypeId       :: T.VarId
       }
   deriving (Eq, Show)
 
 emptyState :: CompilerState
 emptyState =
   CompilerState
-    { _definitions = Map.empty
+    { _valueNames = Map.empty
+    , _valueDefinitions = Map.empty
+    , _typeNames = Map.empty
     , _typeDefinitions = Map.empty
     , _nextTypeId = 0
     }
 
 -- Assorted lenses
 
-definitions :: Lens' CompilerState (Map QualifiedReference TAST.Expr)
-definitions = lens _definitions (\cs mods -> cs {_definitions = mods})
-
-typeDefinitions :: Lens' CompilerState (Map QualifiedReference Type.Type)
-typeDefinitions = lens _typeDefinitions (\cs mods -> cs {_typeDefinitions = mods})
-
-nextTypeId :: Lens' CompilerState Type.VarId
+nextTypeId :: Lens' CompilerState T.VarId
 nextTypeId = lens _nextTypeId (\cs vi -> cs {_nextTypeId = vi})
 
+-- Helpers
+
+-- Lookup a type by name
+lookupType :: CompilerState -> QualifiedReference -> Maybe T.Type
+lookupType cs qvr = do
+  tId <- _typeNames cs !? qvr
+  _typeDefinitions cs !? tId
+
+-- Lookup a value by name
+lookupVal :: CompilerState -> QualifiedReference -> Maybe TAST.Expr
+lookupVal cs qvr = do
+  vId <- _valueNames cs !? qvr
+  _valueDefinitions cs !? vId
+
 -- Not a great name, but
-varIdPlusPlus :: State CompilerState Type.VarId
+varIdPlusPlus :: State CompilerState T.VarId
 varIdPlusPlus = state (\s -> (_nextTypeId s, s {_nextTypeId = _nextTypeId s + 1}))
