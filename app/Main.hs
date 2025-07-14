950af8d4d523fd4bc121830f5ffce391cb5aed14
Jonathan Arnett <j3rn@j3rn.com>
2025-07-14 18:20:23 -0400

    Clear out Main.hs for the time being

--- app/Main.hs
+++ app/Main.hs
@@ -8,24 +8,10 @@ import           Data.Either         (fromRight)
 import qualified Data.Map            as Map
 import qualified Data.Text           as Text
 import qualified Data.Text.IO        as TIO
-import           MNML.CompilerState  (CompilerState (..), Modules, emptyState)
-import           MNML.Generate       (generate)
+import           MNML.CompilerState  (CompilerState (..), emptyState)
 import           System.Directory    (listDirectory)
 import           System.FilePath     (dropExtension, isExtensionOf)
 
 main :: IO ()
 main = do
-  modules <- loadModules "."
-  let state = (emptyState {_modules = modules})
-      result = evalState (generate ("test", "main")) state
-  print result
-
-loadModules :: FilePath -> IO Modules
-loadModules basePath = do
-  projFiles <- findSourceFiles basePath
-  Map.fromList <$> mapM (\file -> (Text.pack (dropExtension file),) <$> TIO.readFile file) projFiles
-
-findSourceFiles :: FilePath -> IO [FilePath]
-findSourceFiles basePath = do
-  files <- fromRight [] <$> (try (listDirectory basePath) :: IO (Either IOError [FilePath]))
-  (filter (isExtensionOf "mnml") files ++) . concat <$> mapM findSourceFiles files
+  putStrLn "Hello, World!"
